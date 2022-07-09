af18e84e00a646912ae839be815104a1360886aa
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2022-07-08 21:22:27 -0300

    core: call example-as-test directly via python instead of bash

--- src/core/model/example-as-test.cc
+++ src/core/model/example-as-test.cc
@@ -86,13 +86,8 @@ ExampleAsTestCase::DoRun (void)
 
   std::stringstream ss;
 
-  // Use bash as shell to allow use of PIPESTATUS
-  #ifdef __WIN32__
-    ss << "python3 ./ns3 run " << m_program;
-  #else
-    ss << "bash -c './ns3 run " << m_program;
-  #endif
-  ss   << " --no-build --command-template=\"" << GetCommandTemplate () << "\""
+  ss << "python3 ./ns3 run " << m_program
+     << " --no-build --command-template=\"" << GetCommandTemplate () << "\""
 
     // redirect std::clog, std::cerr to std::cout
      << " 2>&1 "
@@ -102,12 +97,6 @@ ExampleAsTestCase::DoRun (void)
      << GetPostProcessingCommand ()
      << " > " << testFile;
 
-    // Get the status of ns3
-  #ifdef __WIN32__
-     ss << " && exit %ErrorLevel%";
-  #else
-     ss << " && exit ${PIPESTATUS[0]}'";
-  #endif
   int status = std::system (ss.str ().c_str ());
 
   std::cout << "command:  " << ss.str () << "\n"
