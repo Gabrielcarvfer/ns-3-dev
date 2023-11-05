68f2e118f9eccc654669907b461fcb0ccbf6fbf1
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2023-11-05 15:28:12 -0300

    network: add missing headers for consumers of network

--- src/wifi/test/power-rate-adaptation-test.cc
+++ src/wifi/test/power-rate-adaptation-test.cc
@@ -20,6 +20,7 @@
 #include "ns3/adhoc-wifi-mac.h"
 #include "ns3/boolean.h"
 #include "ns3/constant-position-mobility-model.h"
+#include "ns3/double.h"
 #include "ns3/fcfs-wifi-queue-scheduler.h"
 #include "ns3/frame-exchange-manager.h"
 #include "ns3/interference-helper.h"
