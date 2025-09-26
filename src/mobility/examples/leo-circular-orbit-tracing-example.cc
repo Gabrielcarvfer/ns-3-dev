f8e0fb85a70e153351de9ead74ae2fa32b57b471
thiagomiyazaki <miyathiago@gmail.com>
2025-09-26 16:59:43 +0200

    mobility: Add orbital mobility model and examples

---
+++ src/mobility/examples/leo-circular-orbit-tracing-example.cc
@@ -0,0 +1,74 @@
+// Copyright (c) Tim Schubert
+//
+// SPDX-License-Identifier: GPL-2.0-only
+//
+// Author: Tim Schubert <ns-3-leo@timschubert.net>
+// Porting: Thiago Miyazaki <miyathiago@gmail.com> <t.miyazaki@unesp.br>
+
+#include "ns3/core-module.h"
+#include "ns3/leo-orbit-node-helper.h"
+#include "ns3/mobility-module.h"
+
+#include <fstream>
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("LeoCircularOrbitTracingExample");
+
+void
+CourseChange(std::string context, Ptr<const MobilityModel> position)
+{
+    const Vector pos = position->GetPosition();
+    Ptr<const Node> node = position->GetObject<Node>();
+    std::cout << Simulator::Now() << ":" << node->GetId() << ":" << pos.x << ":" << pos.y << ":"
+              << pos.z << ":" << position->GetVelocity().GetLength() << std::endl;
+}
+
+int
+main(int argc, char* argv[])
+{
+    CommandLine cmd;
+    std::string orbitFile;
+    std::string traceFile;
+    uint32_t duration = 60;    // seconds
+    uint32_t precision = 1000; // milliseconds
+    cmd.AddValue("orbitFile", "CSV file with orbit parameters", orbitFile);
+    cmd.AddValue("traceFile", "CSV file to store mobility trace in", traceFile);
+    cmd.AddValue("precision", "Mobility model time precision in milliseconds", precision);
+    cmd.AddValue("duration", "Duration of the simulation in seconds", duration);
+    cmd.Parse(argc, argv);
+
+    Config::SetDefault("ns3::LeoCircularOrbitMobilityModel::Precision",
+                       TimeValue(MilliSeconds(precision)));
+
+    LeoOrbitNodeHelper orbit(Time(MilliSeconds(precision)));
+    NodeContainer satellites;
+    if (!orbitFile.empty())
+    {
+        satellites = orbit.Install(orbitFile);
+    }
+    else
+    {
+        satellites = orbit.Install({LeoOrbit(1200, 20, 32, 16), LeoOrbit(1180, 30, 12, 10)});
+    }
+
+    Config::Connect("/NodeList/*/$ns3::MobilityModel/CourseChange", MakeCallback(&CourseChange));
+
+    std::streambuf* coutbuf = std::cout.rdbuf();
+    // redirect cout if traceFile is specified
+    std::ofstream out;
+    out.open(traceFile);
+    if (out.is_open())
+    {
+        std::cout.rdbuf(out.rdbuf());
+    }
+
+    std::cout << "Time:Satellite:x:y:z:Speed" << std::endl;
+
+    Simulator::Stop(Time(Seconds(duration)));
+    Simulator::Run();
+    Simulator::Destroy();
+
+    out.close();
+    std::cout.rdbuf(coutbuf);
+}
