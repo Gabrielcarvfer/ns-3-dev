7efe8dbdef8e5d01b827418910fff768a711ff34
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2025-12-22 14:32:29 +0100

    aodv: Update number of internet streams after IPv6 extension stream assignment

--- src/aodv/test/aodv-regression.cc
+++ src/aodv/test/aodv-regression.cc
@@ -196,11 +196,11 @@ ChainRegressionTest::CreateDevices()
     internetStack.Install(*m_nodes);
     streamsUsed += internetStack.AssignStreams(*m_nodes, streamsUsed);
     // InternetStack uses m_size more streams
-    NS_TEST_ASSERT_MSG_EQ(streamsUsed, (devices.GetN() * 5) + m_size, "Stream assignment mismatch");
+    NS_TEST_ASSERT_MSG_EQ(streamsUsed, (devices.GetN() * 8) + m_size, "Stream assignment mismatch");
     streamsUsed += aodv.AssignStreams(*m_nodes, streamsUsed);
     // AODV uses m_size more streams
     NS_TEST_ASSERT_MSG_EQ(streamsUsed,
-                          ((devices.GetN() * 5) + (2 * m_size)),
+                          ((devices.GetN() * 8) + (2 * m_size)),
                           "Stream assignment mismatch");
 
     Ipv4AddressHelper address;
