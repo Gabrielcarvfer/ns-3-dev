82746477cdd95dab89ff36952b30d11fd4023b31
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2026-01-07 11:53:56 +0100

    address https://gitlab.com/nsnam/ns-3-dev/-/merge_requests/2571#note_2970289681

--- src/mobility/model/leo-orbit.h
+++ src/mobility/model/leo-orbit.h
@@ -20,7 +20,20 @@ namespace ns3
 class LeoOrbit;
 
 /**
- * @brief Serialize an orbit
+ * @brief Serialize an orbit parameters in csv format
+ *
+ * Serialized orbit parameters as a comma-separated list in this order:
+ *   - altitude (km, from earth surface)
+ *   - inclination (degrees relative to equator)
+ *   - number of orbital planes
+ *   - number of satellites per plane
+ *
+ * Planes are evenly spaced by 180 degrees divided by the number of planes, starting from the prime
+ * meridian.
+ *
+ * Example: "700,98,2,12" (700 km altitude, 98 degrees of inclination, 2 planes, 12
+ * satellites/plane).
+ *
  * @return the stream
  * @param os the stream
  * @param [in] orbit is an Orbit
@@ -28,7 +41,24 @@ class LeoOrbit;
 std::ostream& operator<<(std::ostream& os, const LeoOrbit& orbit);
 
 /**
- * @brief Deserialize an orbit
+ * @brief Deserialize an orbit in csv format
+ *
+ * Deserialized orbit parameters as a comma-separated list in this order:
+ *   - altitude (km, from earth surface)
+ *   - inclination (degrees relative to equator)
+ *   - number of orbital planes
+ *   - number of satellites per plane
+ *
+ * Planes are evenly spaced by 180 degrees divided by the number of planes, starting from the prime
+ * meridian.
+ *
+ * Example: "700,98,2,12" (700 km altitude, 98 degrees of inclination, 2 planes, 12
+ * satellites/plane).
+ *
+ * If deserialization fails due to unexpected fields/comments/text, the orbit is initialized with
+ * all zero fields. Input stream error flags are cleared to continue parsing, so comments can be
+ * added, except in lines with orbital parameters.
+ *
  * @return the stream
  * @param is the stream
  * @param [out] orbit is the Orbit
