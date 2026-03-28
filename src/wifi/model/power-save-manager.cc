nsnam/ns-3-dev-git

wifi: Move PowerSaveMode attribute from StaWifiMac to PowerSaveManager
Also, make it capable to set the powersave mode for multiple links at
once.

src/wifi/model/power-save-manager.cc
@@ -14,7 +14,11 @@
 #include "wifi-mpdu.h"
 #include "wifi-phy.h"
 
+#include "ns3/attribute-container.h"
+#include "ns3/boolean.h"
 #include "ns3/log.h"
+#include "ns3/pair.h"
+#include "ns3/uinteger.h"
 
 namespace ns3
 {
@@ -26,13 +30,42 @@ NS_OBJECT_ENSURE_REGISTERED(PowerSaveManager);
 TypeId
 PowerSaveManager::GetTypeId()
 {
-    static TypeId tid = TypeId("ns3::PowerSaveManager").SetParent<Object>().SetGroupName("Wifi");
+    static TypeId tid =
+        TypeId("ns3::PowerSaveManager")
+            .SetParent<Object>()
+            .SetGroupName("Wifi")
+            .AddAttribute(
+                "PowerSaveMode",
+                "Enable/disable power save mode on the given links. If a string is used to "
+                "set this attribute, the string must contain a comma-separated list of pairs, "
+                "where each pair is made of the link ID and a boolean value (indicating whether "
+                "to enable PS mode on that link), separated by a space. If this object is not "
+                "initialized yet when this attribute is set, the settings are stored and notified "
+                "to the STA wifi MAC upon initialization.",
+                StringValue(""),
+                MakeAttributeContainerAccessor<PairValue<UintegerValue, BooleanValue>>(
+                    &PowerSaveManager::SetPowerSaveMode),
+                MakeAttributeContainerChecker<PairValue<UintegerValue, BooleanValue>>(
+                    MakePairChecker<UintegerValue, BooleanValue>(MakeUintegerChecker<uint8_t>(),
+                                                                 MakeBooleanChecker())));
     return tid;
 }
 
 PowerSaveManager::~PowerSaveManager()
 {
     NS_LOG_FUNCTION_NOARGS();
+}
+
+void
+PowerSaveManager::DoInitialize()
+{
+    NS_LOG_FUNCTION(this);
+
+    for (const auto& [linkId, enablePs] : m_linkIdEnableMap)
+    {
+        m_staMac->SetPowerSaveMode({enablePs, linkId});
+    }
+    m_linkIdEnableMap.clear();
 }
 
 void
@@ -65,6 +98,22 @@ PowerSaveManager::GetStaInfo(uint8_t linkId)
 {
     NS_ASSERT(m_staInfo.contains(linkId));
     return m_staInfo.at(linkId);
+}
+
+void
+PowerSaveManager::SetPowerSaveMode(const std::map<uint8_t, bool>& linkIdEnableMap)
+{
+    for (const auto& [linkId, enablePs] : linkIdEnableMap)
+    {
+        if (m_staMac && m_staMac->IsInitialized())
+        {
+            m_staMac->SetPowerSaveMode({enablePs, linkId});
+        }
+        else
+        {
+            m_linkIdEnableMap[linkId] = enablePs;
+        }
+    }
 }
 
 void

src/wifi/model/power-save-manager.h
@@ -47,6 +47,15 @@ class PowerSaveManager : public Object
     void SetWifiMac(Ptr<StaWifiMac> mac);
 
     /**
+     * Enable or disable Power Save mode on a given set of links. If this object is not initialized
+     * yet, the settings are recorded and notified to the STA wifi MAC upon initialization.
+     *
+     * @param linkIdEnableMap a link ID-indexed map indicating whether to enable or not power save
+     *                        mode on the link with the given ID
+     */
+    void SetPowerSaveMode(const std::map<uint8_t, bool>& linkIdEnableMap);
+
+    /**
      * Notify that the non-AP STA/MLD has completed association with an AP.
      */
     void NotifyAssocCompleted();
@@ -106,6 +115,7 @@ class PowerSaveManager : public Object
     void TxDropped(WifiMacDropReason reason, Ptr<const WifiMpdu> mpdu);
 
   protected:
+    void DoInitialize() override;
     void DoDispose() override;
 
     /**
@@ -137,6 +147,9 @@ class PowerSaveManager : public Object
   private:
     Ptr<StaWifiMac> m_staMac;             //!< MAC which is using this Power Save Manager
     std::map<uint8_t, StaInfo> m_staInfo; ///< link ID-indexed map of STA infos
+    std::map<uint8_t, bool>
+        m_linkIdEnableMap; ///< a link ID-indexed map indicating whether to enable or not power save
+                           ///< mode on the link with the given ID (used before initialization)
 };
 
 } // namespace ns3

Gabrielcarvfer/ns-3-dev

src/wifi/model/power-save-manager.cc
@@ -14,7 +14,11 @@
 #include "wifi-mpdu.h"
 #include "wifi-phy.h"
 
+#include "ns3/attribute-container.h"
+#include "ns3/boolean.h"
 #include "ns3/log.h"
+#include "ns3/pair.h"
+#include "ns3/uinteger.h"
 
 namespace ns3
 {
@@ -30,6 +34,20 @@ PowerSaveManager::GetTypeId()
         TypeId("ns3::PowerSaveManager")
             .SetParent<Object>()
             .SetGroupName("Wifi")
+            .AddAttribute(
+                "PowerSaveMode",
+                "Enable/disable power save mode on the given links. If a string is used to "
+                "set this attribute, the string must contain a comma-separated list of pairs, "
+                "where each pair is made of the link ID and a boolean value (indicating whether "
+                "to enable PS mode on that link), separated by a space. If this object is not "
+                "initialized yet when this attribute is set, the settings are stored and notified "
+                "to the STA wifi MAC upon initialization.",
+                StringValue(""),
+                MakeAttributeContainerAccessor<PairValue<UintegerValue, BooleanValue>>(
+                    &PowerSaveManager::SetPowerSaveMode),
+                MakeAttributeContainerChecker<PairValue<UintegerValue, BooleanValue>>(
+                    MakePairChecker<UintegerValue, BooleanValue>(MakeUintegerChecker<uint8_t>(),
+                                                                 MakeBooleanChecker())))
             .AddTraceSource(
                 "PmMode",
                 "Traces every change in the power management mode of the STAs affiliated with this "
@@ -43,6 +61,18 @@ PowerSaveManager::GetTypeId()
 PowerSaveManager::~PowerSaveManager()
 {
     NS_LOG_FUNCTION_NOARGS();
+}
+
+void
+PowerSaveManager::DoInitialize()
+{
+    NS_LOG_FUNCTION(this);
+
+    for (const auto& [linkId, enablePs] : m_linkIdEnableMap)
+    {
+        m_staMac->SetPowerSaveMode({enablePs, linkId});
+    }
+    m_linkIdEnableMap.clear();
 }
 
 void
@@ -75,6 +105,22 @@ PowerSaveManager::GetStaInfo(linkId_t linkId)
 {
     NS_ASSERT(m_staInfo.contains(linkId));
     return m_staInfo.at(linkId);
+}
+
+void
+PowerSaveManager::SetPowerSaveMode(const std::map<linkId_t, bool>& linkIdEnableMap)
+{
+    for (const auto& [linkId, enablePs] : linkIdEnableMap)
+    {
+        if (m_staMac && m_staMac->IsInitialized())
+        {
+            m_staMac->SetPowerSaveMode({enablePs, linkId});
+        }
+        else
+        {
+            m_linkIdEnableMap[linkId] = enablePs;
+        }
+    }
 }
 
 void

src/wifi/model/power-save-manager.h
@@ -50,6 +50,15 @@ class PowerSaveManager : public Object
     void SetWifiMac(Ptr<StaWifiMac> mac);
 
     /**
+     * Enable or disable Power Save mode on a given set of links. If this object is not initialized
+     * yet, the settings are recorded and notified to the STA wifi MAC upon initialization.
+     *
+     * @param linkIdEnableMap a link ID-indexed map indicating whether to enable or not power save
+     *                        mode on the link with the given ID
+     */
+    void SetPowerSaveMode(const std::map<linkId_t, bool>& linkIdEnableMap);
+
+    /**
      * Notify that the non-AP STA/MLD has completed association with an AP.
      */
     void NotifyAssocCompleted();
@@ -109,6 +118,7 @@ class PowerSaveManager : public Object
     void TxDropped(WifiMacDropReason reason, Ptr<const WifiMpdu> mpdu);
 
   protected:
+    void DoInitialize() override;
     void DoDispose() override;
 
     /**
@@ -140,6 +150,9 @@ class PowerSaveManager : public Object
   private:
     Ptr<StaWifiMac> m_staMac;              //!< MAC which is using this Power Save Manager
     std::map<linkId_t, StaInfo> m_staInfo; ///< link ID-indexed map of STA infos
+    std::map<linkId_t, bool>
+        m_linkIdEnableMap; ///< a link ID-indexed map indicating whether to enable or not power save
+                           ///< mode on the link with the given ID (used before initialization)
     TracedCallback<linkId_t, WifiPowerManagementMode>
         m_pmModeLogger; ///< link ID-indexed power management mode logger
 