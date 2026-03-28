nsnam/ns-3-dev-git

wifi: A STA in PS mode goes to sleep (if needed) after releasing the channel

src/wifi/model/default-power-save-manager.cc
@@ -179,6 +179,24 @@ DefaultPowerSaveManager::DoNotifyRequestAccess(Ptr<Txop> txop, uint8_t linkId)
 }
 
 void
+DefaultPowerSaveManager::DoNotifyChannelReleased(Ptr<Txop> txop, uint8_t linkId)
+{
+    NS_LOG_FUNCTION(this << txop << linkId);
+
+    if (GetStaMac()->GetPmMode(linkId) == WifiPowerManagementMode::WIFI_PM_ACTIVE)
+    {
+        // cancel timer, if any
+        if (auto it = m_wakeUpEvents.find(linkId); it != m_wakeUpEvents.cend())
+        {
+            it->second.Cancel();
+        }
+        return;
+    }
+
+    GoToSleepIfPossible(linkId);
+}
+
+void
 DefaultPowerSaveManager::DoTxDropped(WifiMacDropReason reason, Ptr<const WifiMpdu> mpdu)
 {
     NS_LOG_FUNCTION(this << reason << *mpdu);

src/wifi/model/default-power-save-manager.h
@@ -45,6 +45,7 @@ class DefaultPowerSaveManager : public PowerSaveManager
     void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, uint8_t linkId) override;
     void DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, uint8_t linkId) override;
     void DoNotifyRequestAccess(Ptr<Txop> txop, uint8_t linkId) override;
+    void DoNotifyChannelReleased(Ptr<Txop> txop, uint8_t linkId) override;
     void DoTxDropped(WifiMacDropReason reason, Ptr<const WifiMpdu> mpdu) override;
 
     std::map<uint8_t, EventId> m_wakeUpEvents; ///< events scheduled to wake up PHYs

src/wifi/model/power-save-manager.cc
@@ -275,6 +275,14 @@ PowerSaveManager::NotifyRequestAccess(Ptr<Txop> txop, uint8_t linkId)
 }
 
 void
+PowerSaveManager::NotifyChannelReleased(Ptr<Txop> txop, uint8_t linkId)
+{
+    NS_LOG_FUNCTION(this << txop << linkId);
+
+    DoNotifyChannelReleased(txop, linkId);
+}
+
+void
 PowerSaveManager::TxDropped(WifiMacDropReason reason, Ptr<const WifiMpdu> mpdu)
 {
     NS_LOG_FUNCTION(this << reason << *mpdu);

src/wifi/model/power-save-manager.h
@@ -111,6 +111,14 @@ class PowerSaveManager : public Object
     void NotifyRequestAccess(Ptr<Txop> txop, uint8_t linkId);
 
     /**
+     * Notify that the given TXOP has released the channel on the given link.
+     *
+     * @param txop the DCF/EDCAF releasing the channel
+     * @param linkId the ID of the given link
+     */
+    void NotifyChannelReleased(Ptr<Txop> txop, uint8_t linkId);
+
+    /**
      * Notify that the given MPDU has been discarded for the given reason.
      *
      * @param reason the reason why the MPDU was dropped
@@ -200,6 +208,14 @@ class PowerSaveManager : public Object
      * @param linkId the ID of the given link
      */
     virtual void DoNotifyRequestAccess(Ptr<Txop> txop, uint8_t linkId) = 0;
+
+    /**
+     * Notify subclasses that the given TXOP is releasing the channel on the given link.
+     *
+     * @param txop the DCF/EDCAF releasing the channel
+     * @param linkId the ID of the given link
+     */
+    virtual void DoNotifyChannelReleased(Ptr<Txop> txop, uint8_t linkId) = 0;
 
     /**
      * Notify subclasses that the given MPDU has been discarded for the given reason.

Gabrielcarvfer/ns-3-dev

src/wifi/model/default-power-save-manager.cc
@@ -195,6 +195,14 @@ DefaultPowerSaveManager::DoNotifyRequestAccess(Ptr<Txop> txop, linkId_t linkId)
 }
 
 void
+DefaultPowerSaveManager::DoNotifyChannelReleased(Ptr<Txop> txop, linkId_t linkId)
+{
+    NS_LOG_FUNCTION(this << txop << linkId);
+
+    GoToSleepIfPossible(linkId);
+}
+
+void
 DefaultPowerSaveManager::DoTxDropped(WifiMacDropReason reason, Ptr<const WifiMpdu> mpdu)
 {
     NS_LOG_FUNCTION(this << reason << *mpdu);

src/wifi/model/default-power-save-manager.h
@@ -46,6 +46,7 @@ class DefaultPowerSaveManager : public PowerSaveManager
     void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, linkId_t linkId) override;
     void DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, linkId_t linkId) override;
     void DoNotifyRequestAccess(Ptr<Txop> txop, linkId_t linkId) override;
+    void DoNotifyChannelReleased(Ptr<Txop> txop, linkId_t linkId) override;
     void DoTxDropped(WifiMacDropReason reason, Ptr<const WifiMpdu> mpdu) override;
 
     std::map<linkId_t, EventId> m_wakeUpEvents; ///< events scheduled to wake up PHYs

src/wifi/model/power-save-manager.cc
@@ -286,6 +286,14 @@ PowerSaveManager::NotifyRequestAccess(Ptr<Txop> txop, linkId_t linkId)
 }
 
 void
+PowerSaveManager::NotifyChannelReleased(Ptr<Txop> txop, linkId_t linkId)
+{
+    NS_LOG_FUNCTION(this << txop << linkId);
+
+    DoNotifyChannelReleased(txop, linkId);
+}
+
+void
 PowerSaveManager::TxDropped(WifiMacDropReason reason, Ptr<const WifiMpdu> mpdu)
 {
     NS_LOG_FUNCTION(this << reason << *mpdu);

src/wifi/model/power-save-manager.h
@@ -114,6 +114,14 @@ class PowerSaveManager : public Object
     void NotifyRequestAccess(Ptr<Txop> txop, linkId_t linkId);
 
     /**
+     * Notify that the given TXOP has released the channel on the given link.
+     *
+     * @param txop the DCF/EDCAF releasing the channel
+     * @param linkId the ID of the given link
+     */
+    void NotifyChannelReleased(Ptr<Txop> txop, linkId_t linkId);
+
+    /**
      * Notify that the given MPDU has been discarded for the given reason.
      *
      * @param reason the reason why the MPDU was dropped
@@ -212,6 +220,14 @@ class PowerSaveManager : public Object
      * @param linkId the ID of the given link
      */
     virtual void DoNotifyRequestAccess(Ptr<Txop> txop, linkId_t linkId) = 0;
+
+    /**
+     * Notify subclasses that the given TXOP is releasing the channel on the given link.
+     *
+     * @param txop the DCF/EDCAF releasing the channel
+     * @param linkId the ID of the given link
+     */
+    virtual void DoNotifyChannelReleased(Ptr<Txop> txop, linkId_t linkId) = 0;
 
     /**
      * Notify subclasses that the given MPDU has been discarded for the given reason.