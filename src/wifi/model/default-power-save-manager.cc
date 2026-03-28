nsnam/ns-3-dev-git

wifi: Put PHY to sleep or enqueue PS Poll when receiving a Beacon frame

src/wifi/model/default-power-save-manager.cc
@@ -9,8 +9,10 @@
 #include "default-power-save-manager.h"
 
 #include "sta-wifi-mac.h"
+#include "wifi-phy.h"
 
 #include "ns3/log.h"
+#include "ns3/simulator.h"
 
 namespace ns3
 {
@@ -37,6 +39,82 @@ DefaultPowerSaveManager::DefaultPowerSaveManager()
 DefaultPowerSaveManager::~DefaultPowerSaveManager()
 {
     NS_LOG_FUNCTION_NOARGS();
+}
+
+void
+DefaultPowerSaveManager::GoToSleepIfPossible(uint8_t linkId)
+{
+    NS_LOG_FUNCTION(this << linkId);
+
+    if (!GetStaMac()->IsAssociated())
+    {
+        NS_LOG_DEBUG("STA is not associated");
+        return;
+    }
+
+    if (GetStaMac()->GetPmMode(linkId) == WIFI_PM_ACTIVE)
+    {
+        NS_LOG_DEBUG("STA on link " << +linkId << " is in active mode");
+        return;
+    }
+
+    if (GetStaMac()->GetWifiPhy(linkId)->IsStateSleep())
+    {
+        NS_LOG_DEBUG("PHY operating on link " << +linkId << " is already in sleep state");
+        return;
+    }
+
+    auto& staInfo = GetStaInfo(linkId);
+
+    if (staInfo.beaconInterval.IsZero() || staInfo.lastBeaconTimestamp.IsZero())
+    {
+        NS_LOG_DEBUG("No Beacon received yet, cannot put PHY to sleep");
+        return;
+    }
+
+    // if nothing to do, put PHY to sleep
+    if (!staInfo.pendingUnicast && !staInfo.pendingGroupcast && !HasFramesToSend(linkId))
+    {
+        NS_LOG_DEBUG("PHY operating on link " << +linkId << " is put to sleep");
+        GetStaMac()->GetWifiPhy(linkId)->SetSleepMode(true);
+
+        if (auto it = m_wakeUpEvents.find(linkId); it != m_wakeUpEvents.cend())
+        {
+            it->second.Cancel();
+        }
+
+        // schedule wakeup before next Beacon frame in listen interval periods
+        NS_ASSERT(GetListenInterval() > 0);
+
+        auto delay = (Simulator::Now() - staInfo.lastBeaconTimestamp) % staInfo.beaconInterval;
+        delay = staInfo.beaconInterval - delay;
+        delay += (GetListenInterval() - 1) * staInfo.beaconInterval;
+
+        NS_LOG_DEBUG("Scheduling PHY on link " << +linkId << " to wake up in "
+                                               << delay.As(Time::US));
+        m_wakeUpEvents[linkId] =
+            Simulator::Schedule(delay, &WifiPhy::ResumeFromSleep, GetStaMac()->GetWifiPhy(linkId));
+    }
+}
+
+void
+DefaultPowerSaveManager::DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, uint8_t linkId)
+{
+    NS_LOG_FUNCTION(this << beacon << linkId);
+
+    if (GetStaMac()->GetPmMode(linkId) != WIFI_PM_POWERSAVE)
+    {
+        return;
+    }
+
+    if (GetStaInfo(linkId).pendingUnicast)
+    {
+        GetStaMac()->EnqueuePsPoll(linkId);
+    }
+    else
+    {
+        GoToSleepIfPossible(linkId);
+    }
 }
 
 } // namespace ns3

src/wifi/model/default-power-save-manager.h
@@ -30,6 +30,18 @@ class DefaultPowerSaveManager : public PowerSaveManager
 
     DefaultPowerSaveManager();
     ~DefaultPowerSaveManager() override;
+
+  protected:
+    /**
+     * Put the PHY operating on the given link to sleep, if no reason to stay awake.
+     *
+     * @param linkId the ID of the given link
+     */
+    void GoToSleepIfPossible(uint8_t linkId);
+
+    void DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, uint8_t linkId) override;
+
+    std::map<uint8_t, EventId> m_wakeUpEvents; ///< events scheduled to wake up PHYs
 };
 
 } // namespace ns3

Gabrielcarvfer/ns-3-dev

src/wifi/model/default-power-save-manager.cc
@@ -9,8 +9,10 @@
 #include "default-power-save-manager.h"
 
 #include "sta-wifi-mac.h"
+#include "wifi-phy.h"
 
 #include "ns3/log.h"
+#include "ns3/simulator.h"
 
 namespace ns3
 {
@@ -37,6 +39,83 @@ DefaultPowerSaveManager::DefaultPowerSaveManager()
 DefaultPowerSaveManager::~DefaultPowerSaveManager()
 {
     NS_LOG_FUNCTION_NOARGS();
+}
+
+void
+DefaultPowerSaveManager::GoToSleepIfPossible(linkId_t linkId)
+{
+    NS_LOG_FUNCTION(this << linkId);
+
+    if (!GetStaMac()->IsAssociated())
+    {
+        NS_LOG_DEBUG("STA is not associated");
+        return;
+    }
+
+    if (GetStaMac()->GetPmMode(linkId) == WIFI_PM_ACTIVE)
+    {
+        NS_LOG_DEBUG("STA on link " << +linkId << " is in active mode");
+        return;
+    }
+
+    if (GetStaMac()->GetWifiPhy(linkId)->IsStateSleep())
+    {
+        NS_LOG_DEBUG("PHY operating on link " << +linkId << " is already in sleep state");
+        return;
+    }
+
+    auto& staInfo = GetStaInfo(linkId);
+
+    if (staInfo.beaconInterval.IsZero() || staInfo.lastBeaconTimestamp.IsZero())
+    {
+        NS_LOG_DEBUG("No Beacon received yet, cannot put PHY to sleep");
+        return;
+    }
+
+    // if nothing to do, put PHY to sleep
+    if (!staInfo.pendingUnicast && !staInfo.pendingGroupcast &&
+        !HasRequestedOrGainedChannel(linkId))
+    {
+        NS_LOG_DEBUG("PHY operating on link " << +linkId << " is put to sleep");
+        GetStaMac()->GetWifiPhy(linkId)->SetSleepMode(true);
+
+        if (auto it = m_wakeUpEvents.find(linkId); it != m_wakeUpEvents.cend())
+        {
+            it->second.Cancel();
+        }
+
+        // schedule wakeup before next Beacon frame in listen interval periods
+        NS_ASSERT(GetListenInterval() > 0);
+
+        auto delay = (Simulator::Now() - staInfo.lastBeaconTimestamp) % staInfo.beaconInterval;
+        delay = staInfo.beaconInterval - delay;
+        delay += (GetListenInterval() - 1) * staInfo.beaconInterval;
+
+        NS_LOG_DEBUG("Scheduling PHY on link " << +linkId << " to wake up in "
+                                               << delay.As(Time::US));
+        m_wakeUpEvents[linkId] =
+            Simulator::Schedule(delay, &WifiPhy::ResumeFromSleep, GetStaMac()->GetWifiPhy(linkId));
+    }
+}
+
+void
+DefaultPowerSaveManager::DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, linkId_t linkId)
+{
+    NS_LOG_FUNCTION(this << beacon << linkId);
+
+    if (GetStaMac()->GetPmMode(linkId) != WIFI_PM_POWERSAVE)
+    {
+        return;
+    }
+
+    if (GetStaInfo(linkId).pendingUnicast)
+    {
+        GetStaMac()->EnqueuePsPoll(linkId);
+    }
+    else
+    {
+        GoToSleepIfPossible(linkId);
+    }
 }
 
 } // namespace ns3

src/wifi/model/default-power-save-manager.h
@@ -30,6 +30,18 @@ class DefaultPowerSaveManager : public PowerSaveManager
 
     DefaultPowerSaveManager();
     ~DefaultPowerSaveManager() override;
+
+  protected:
+    /**
+     * Put the PHY operating on the given link to sleep, if no reason to stay awake.
+     *
+     * @param linkId the ID of the given link
+     */
+    void GoToSleepIfPossible(linkId_t linkId);
+
+    void DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, linkId_t linkId) override;
+
+    std::map<linkId_t, EventId> m_wakeUpEvents; ///< events scheduled to wake up PHYs
 };
 
 } // namespace ns3