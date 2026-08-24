nsnam/ns-3-dev-git

wifi: Add a function to send Disassociation frames

src/wifi/model/mgt-headers.cc
@@ -515,4 +515,52 @@ MgtAssocResponseHeader::DeserializeFromPerStaProfileImpl(Buffer::Iterator start,
                           DeserializeFromPerStaProfileImpl(i, length - distance, frame);
 }
 
+/***********************************************************
+ *          Disassociation
+ ***********************************************************/
+
+NS_OBJECT_ENSURE_REGISTERED(MgtDisassociationHeader);
+
+TypeId
+MgtDisassociationHeader::GetTypeId()
+{
+    static TypeId tid = TypeId("ns3::MgtDisassociationHeader")
+                            .SetParent<Header>()
+                            .SetGroupName("Wifi")
+                            .AddConstructor<MgtDisassociationHeader>();
+    return tid;
+}
+
+TypeId
+MgtDisassociationHeader::GetInstanceTypeId() const
+{
+    return GetTypeId();
+}
+
+uint32_t
+MgtDisassociationHeader::GetSerializedSize() const
+{
+    return 2; // reason code size
+}
+
+void
+MgtDisassociationHeader::Serialize(Buffer::Iterator start) const
+{
+    start.WriteHtolsbU16(m_reasonCode);
+}
+
+uint32_t
+MgtDisassociationHeader::Deserialize(Buffer::Iterator start)
+{
+    auto i = start;
+    m_reasonCode = i.ReadLsbtohU16();
+    return i.GetDistanceFrom(start);
+}
+
+void
+MgtDisassociationHeader::Print(std::ostream& os) const
+{
+    os << "reason code=" << m_reasonCode;
+}
+
 } // namespace ns3

Gabrielcarvfer/ns-3-dev

src/wifi/model/mgt-headers.cc
@@ -558,6 +558,54 @@ MgtAssocResponseHeader::DeserializeFromPerStaProfileImpl(Buffer::Iterator start,
                           DeserializeFromPerStaProfileImpl(i, length - distance, frame);
 }
 
+/***********************************************************
+ *          Disassociation
+ ***********************************************************/
+
+NS_OBJECT_ENSURE_REGISTERED(MgtDisassociationHeader);
+
+TypeId
+MgtDisassociationHeader::GetTypeId()
+{
+    static TypeId tid = TypeId("ns3::MgtDisassociationHeader")
+                            .SetParent<Header>()
+                            .SetGroupName("Wifi")
+                            .AddConstructor<MgtDisassociationHeader>();
+    return tid;
+}
+
+TypeId
+MgtDisassociationHeader::GetInstanceTypeId() const
+{
+    return GetTypeId();
+}
+
+uint32_t
+MgtDisassociationHeader::GetSerializedSize() const
+{
+    return 2; // reason code size
+}
+
+void
+MgtDisassociationHeader::Serialize(Buffer::Iterator start) const
+{
+    start.WriteU16(m_reasonCode);
+}
+
+uint32_t
+MgtDisassociationHeader::Deserialize(Buffer::Iterator start)
+{
+    auto i = start;
+    m_reasonCode = i.ReadU16();
+    return i.GetDistanceFrom(start);
+}
+
+void
+MgtDisassociationHeader::Print(std::ostream& os) const
+{
+    os << "reason code=" << m_reasonCode;
+}
+
 // Explicit instantiation definitions; see the extern template declarations in
 // mgt-headers.h (active on Linux/macOS only; see comment there for rationale).
 #if !defined(_WIN32)