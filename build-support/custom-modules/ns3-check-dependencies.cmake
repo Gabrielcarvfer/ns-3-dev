0e7dda50f681d1526d058bf0bfdb16c5bab2ceb1
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2026-09-25 15:14:10 +0200

    build: Import each Python dependency only once when checking it

--- build-support/custom-modules/ns3-check-dependencies.cmake
+++ build-support/custom-modules/ns3-check-dependencies.cmake
@@ -34,32 +34,34 @@ function(check_deps missing_deps)
   endforeach()
 
   foreach(package ${DEPS_PYTHON_PACKAGES})
+    # Import each package only once, as importing some of them (e.g. cppyy) is
+    # slow
     execute_process(
-      COMMAND ${Python3_EXECUTABLE} -c "import ${package}"
-      RESULT_VARIABLE return_code OUTPUT_QUIET ERROR_QUIET
+      COMMAND
+        ${Python3_EXECUTABLE} -c
+        "import os; import ${package}; print(os.path.abspath(os.path.dirname(${package}.__file__)))"
+      RESULT_VARIABLE return_code
+      OUTPUT_VARIABLE venv_site_packages_path
+      ERROR_QUIET
     )
     if(NOT (${return_code} EQUAL 0))
       list(APPEND local_missing_deps ${package})
     else()
       # To make sure CMake import files can be found from venv site packages, we
       # manually add them to CMAKE_PREFIX_PATH
-      execute_process(
-        COMMAND
-          ${Python3_EXECUTABLE} -c
-          "import os; import ${package}; print(os.path.abspath(os.path.dirname(${package}.__file__)))"
-        OUTPUT_VARIABLE venv_site_packages_path
-      )
+
       # Remove newlines (\n, \r, \r\n)
       string(REGEX REPLACE "[\r\n]+$" "" venv_site_packages_path
                            "${venv_site_packages_path}"
       )
-      if(EXISTS ${venv_site_packages_path})
-        if(NOT (DEFINED CMAKE_PREFIX_PATH))
-          set(CMAKE_PREFIX_PATH "" PARENT_SCOPE)
-        endif()
-        set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};${venv_site_packages_path}"
-            PARENT_SCOPE
-        )
+      # Keep only the last line, as importing a package may print messages (e.g.
+      # cppyy rebuilding its pre-compiled header)
+      string(REGEX MATCH "[^\r\n]*$" venv_site_packages_path
+                   "${venv_site_packages_path}"
+      )
+      if(EXISTS "${venv_site_packages_path}")
+        list(APPEND CMAKE_PREFIX_PATH ${venv_site_packages_path})
+        set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH}" PARENT_SCOPE)
       endif()
     endif()
   endforeach()
